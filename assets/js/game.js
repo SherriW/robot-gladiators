75af54a4043ff1a6de07cc87ea6835bce7907f62
Sherri Winslow <swinslowcoding@gmail.com>
2021-09-13 03:19:39 -0700

    added alert to check javascript, variable pronpt for user input, function to fight, and called function

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// Alert
+window.alert("This is an alert! JavaScript is running!");
+
+// variable to Prompt for user input
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName)
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// Calling the function fight
+//fight();
+
