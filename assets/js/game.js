4082056d8f96d7c0a99f027d35c1b7e04d60ce41
Sherri Winslow <swinslowcoding@gmail.com>
2021-09-17 12:44:57 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,13 @@ console.log (enemyNames.length);
 console.log (enemyNames[0]);
 console.log (enemyNames[3]);
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min +1) + min);
+
+  return value;
+};
+
 // fight function (now with parameter for enemy's name)
 var fight = function (enemyName) {
   // repeat and execute as long as the enemy-robot is alive
@@ -29,26 +36,20 @@ var fight = function (enemyName) {
       // if yes (true_, leave right
       if (confirmSkip) {
         window.alert (
-          playerName + ' has declined to skip this fight. Goodbye!'
-        );
+          playerName + ' has declined to skip this fight. Goodbye!');
         // subtract mnoney from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log ('playerMoney', playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtractin the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log (
-      playerName +
-        'attacked ' +
-        enemyName +
-        ', ' +
-        enemyName +
-        ' now has ' +
-        enemyHealth +
-        ' health remaining.'
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+    console.log(
+      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
 
     // check enemy's health
@@ -64,8 +65,11 @@ var fight = function (enemyName) {
       window.alert (enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // remove players's health by subtracting the amount set in the enemy.attack variable
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+
     console.log (
       enemyName +
         ' attacked ' +
@@ -108,7 +112,8 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth berfore starting new fight
-      enemyHealth = 50;
+      // function to generate random numeric value
+      enemyHealth = randomNumber(40, 60)
 
       // pass the pickedenemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight (pickedEnemyName);
